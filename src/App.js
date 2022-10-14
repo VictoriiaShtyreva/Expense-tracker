aa7ca0a91a4aa800b1b1f4652fea5faba963b564
Viktoriia <vika1411akva0903@icloud.com>
2022-10-14 12:09:29 +0300

    add: new folders

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Expenses from './components/Expenses';
+
+function App() {
+  const expenses = [
+    {title: 'Car Insurance', amount: 250, date: new Date(2021, 3,15)},
+    {title: 'Store', amount: 50, date: new Date(2021, 3,25)},
+    {title: 'Shopping', amount: 450, date: new Date(2021, 5,10)},
+  ];
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
+    </div>
+  );
+}
+
+export default App;
