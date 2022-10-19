a670850dfe3037b54d8b7ffad338fff76a4cf737
Viktoriia <vika1411akva0903@icloud.com>
2022-10-19 20:15:20 +0300

    feat: add new components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 const App = () => {
   const expenses = [
-    {title: 'Car Insurance', amount: 250, date: new Date(2021, 3,15)},
-    {title: 'Store', amount: 50, date: new Date(2021, 3,25)},
-    {title: 'Shopping', amount: 450, date: new Date(2021, 5,10)},
+    {id:'1' , title: 'Car Insurance', amount: 250, date: new Date(2021, 3,15)},
+    {id:'2' , title: 'Pharmacy', amount: 50, date: new Date(2021, 3,25)},
+    {id:'3' , title: 'Shopping', amount: 450, date: new Date(2021, 5,10)},
+    {id:'4' , title: 'Book', amount: 48, date: new Date(2021, 8,14)},
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   return (
     <div>
+      <NewExpense onAddExpenseHandler={addExpenseHandler}/>
       <Expenses items={expenses}/>
     </div>
   );
